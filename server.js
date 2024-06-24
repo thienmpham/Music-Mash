83a3d8f741199275041a834b6c8f10044952b914
thienmpham <thienmpham@gmail.com>
2024-06-24 11:54:42 -0400

    Setting up auth2.0

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const client_secret = process.env.CLIENT_SECRET;
 const AUTHORIZE = 'https://accounts.spotify.com/authorize';
 const redirect_uri = 'http://127.0.0.1:5500/home.html';
 
+const AUTH_URL = `https://accounts.spotify.com/authorize?
+client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}
+scrope=user-read-currently-playing%20streaming%20playlist-read-private%20
+playlist-modify-private%20playlist-modify-public%20user-follow-read%20
+user-top-read%20user-library-modify%20user-library-read%20user-read-email%20
+user-read-private%20`
 console.log(client_id);
 
-function requestAuthorization() {
-    let url = AUTHORIZE;
-    url += "?client_id=" + client_id;
-    url += "response_type=code";
-    url += "&redirect_uri=" + redirect_uri;
-    url += "&show_dialog=true";
-    url += "&scope"
-}
